8b40accc66fa7d18e56070c2faf0551dd66a3ee9
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:27:45 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan6/Mamalia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan6;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : PBO1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program kambing
+ * static
+ * 
+ */
+public class Mamalia {
+    // Variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+}
